6acfd442a4759e146e64ff37ccbf9cdb9d9eb16f
Your Name <you@example.com>
2024-08-27 00:39:19 +0200

    fix: path

--- apps/api/Program.cs
+++ apps/api/Program.cs
@@ -7,9 +7,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt =>
-{
-  opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+
+  opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 var app = builder.Build();
 
